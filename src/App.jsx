4a42d698e92d4cf81cf6de04a543ff48462b347d
Orisabiyi <orisabiyidavid@gmail.com>
2024-06-27 13:40:15 +0100

    changed the linheight of text globally, added the path to About and updated the style for AboutUs

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import AboutUs from "./components/AboutUs";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="pricing" element={<Pricing />} />
+        <Route path="/about" element={<AboutUs />} />
+        {/*
         <Route path="product" element={<Product />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />} />
