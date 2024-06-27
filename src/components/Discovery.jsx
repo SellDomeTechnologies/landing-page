fff089e83f04a80a0a998ff0bb81fba0314cb8a7
Orisabiyi <orisabiyidavid@gmail.com>
2024-06-27 15:42:01 +0100

    included the content for the Discovery component

---
+++ src/components/Discovery.jsx
@@ -0,0 +1,22 @@
+import styles from "./Discovery.module.css";
+
+function Discovery() {
+  return (
+    <section className={styles.section__discovery}>
+      <div>
+        <h2>Effortless Vendor Discovery</h2>
+        <p>
+          Say goodbye to endless searches and countless hours spent sourcing
+          materials. SellDome connects fashion designers with a curated network
+          of reputable vendors, making it simple to find the perfect fabrics,
+          trims, and accessories for your designs.
+        </p>
+      </div>
+      <div>
+        <img src="iphone.png" alt="iphone logo" />
+      </div>
+    </section>
+  );
+}
+
+export default Discovery;
