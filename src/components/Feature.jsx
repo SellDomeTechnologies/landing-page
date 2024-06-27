6959b1baabc4d1cad39dc13716b2309017f2a688
Orisabiyi <orisabiyidavid@gmail.com>
2024-06-27 17:25:42 +0100

    added an array and object to display list of key features

---
+++ src/components/Feature.jsx
@@ -0,0 +1,34 @@
+const data = [
+  {
+    title: "We offer best services",
+    content: "Lorem Ipsum is not simply random text",
+    img: "",
+  },
+  {
+    title: "Search",
+    content: "It has roots in a piece of classical",
+  },
+  {
+    title: "Connect",
+    content: "Lorem Ipsum is not simply random text",
+  },
+];
+
+function Feature() {
+  return (
+    <section>
+      <div>
+        <h2>Key Features</h2>
+        <h1>We offer best services</h1>
+        <p>
+          Contrary to popular belief, Lorem Ipsum is not simply random text. It
+          has roots in a piece of classical Latin literature from 45 BC.
+        </p>
+      </div>
+
+      <div></div>
+    </section>
+  );
+}
+
+export default Feature;
