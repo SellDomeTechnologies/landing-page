0c67cd390dd3465de950353a7cd81d92b867c37d
Orisabiyi <orisabiyidavid@gmail.com>
2024-06-27 09:46:49 +0100

    link layout for the page

---
+++ src/components/PageNav.jsx
@@ -0,0 +1,24 @@
+import { NavLink } from "react-router-dom";
+
+function PageNav() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about">About us</NavLink>
+        </li>
+        <li>
+          <NavLink to="/pricing">Pricing</NavLink>
+        </li>
+        <li>
+          <NavLink to="/resources">Resources</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default PageNav;
