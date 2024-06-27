dc3d2796ee73d788b2131ad33b8ea07c8c14ee23
Orisabiyi <orisabiyidavid@gmail.com>
2024-06-27 12:43:02 +0100

    included the styles in the Home pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import PageNav from "../components/PageNav";
+import styles from "./Home.module.css";
 
 function Home() {
   return (
     <>
       <PageNav />
-      <header></header>
+      <header className={styles.header}></header>
     </>
   );
 }
