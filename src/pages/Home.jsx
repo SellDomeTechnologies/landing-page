cd7dd183463825c8ec84d77944ba57479c26aa98
Orisabiyi <orisabiyidavid@gmail.com>
2024-06-27 12:48:55 +0100

    wrote the page content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,21 @@ function Home() {
   return (
     <>
       <PageNav />
-      <header className={styles.header}></header>
+      <header className={styles.header}>
+        <h1>
+          Welcome
+          <br />
+          to SellDome
+        </h1>
+
+        <h4>
+          Your Ultimate Fashion Marketplace
+          <br />
+          Discover. Connect. Create.
+        </h4>
+
+        <button>Get Started</button>
+      </header>
     </>
   );
 }
