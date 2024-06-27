bd4af6fbadec16a24039afca862903b870c19753
Orisabiyi <orisabiyidavid@gmail.com>
2024-06-27 15:32:59 +0100

    updated the structure of the Download component and styled the elements

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -1,22 +1,26 @@
+import styles from "./Download.module.css";
+
 function Download() {
   return (
-    <section>
-      <div>
+    <section className={styles.section__download}>
+      <div className={styles.download__cloth}>
         <img src="cloth.png" alt="clothing image" />
       </div>
-      <div>
+      <div className={styles.download__content}>
         <h3>
           With our innovative moble app, finding the perfect materials and
           accessories for your next masterpiece have never been easier.
         </h3>
         <p>Download the SellDome App Now and Start Creating!</p>
         <p>Available on the App Store and Google Play.</p>
-        <button>
-          <img src="app-store.png" alt="download btn" />
-        </button>
-        <button>
-          <img src="app-store.png" alt="download btn" />
-        </button>
+        <div className={styles.download__button}>
+          <button>
+            <img src="app-store.png" alt="download btn" />
+          </button>
+          <button>
+            <img src="google-play.png" alt="download btn" />
+          </button>
+        </div>
       </div>
     </section>
   );
