bdaacf2bb36c2915779ea035b699c7d82fdf833e
Orisabiyi <orisabiyidavid@gmail.com>
2024-06-27 13:19:09 +0100

    included the Aboutus content

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,5 +1,13 @@
 function AboutUs() {
-  return <section></section>;
+  return (
+    <section>
+      <p>
+        Selldome is revolutionizing the fashion industry by providing a seamless
+        platform for fashion <br /> designers and vendors to connect,
+        collaborate, and create stunning designs.
+      </p>
+    </section>
+  );
 }
 
 export default AboutUs;
