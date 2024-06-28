2a03587bc00e41446323dd27d451f9af17bfe486
Orisabiyi <orisabiyidavid@gmail.com>
2024-06-28 09:54:21 +0100

    styled the section of today component and included it

---
+++ src/components/Today.jsx
@@ -0,0 +1,20 @@
+import styles from "./Today.module.css";
+
+function Today() {
+  return (
+    <section className={styles.section__today}>
+      <h2>Join Us Today</h2>
+      <p>
+        Join SellDome Today and Elevate Your Fashion Game! Ready to take your
+        fashion journey to new heights? Join SellDome today and experience the
+        future of fashion sourcing.
+        <br />
+        Whether you're a vendor looking to expand your reach or a fashion
+        designer in search of inspiration, SellDome has everything you need to
+        succeed.
+      </p>
+    </section>
+  );
+}
+
+export default Today;
