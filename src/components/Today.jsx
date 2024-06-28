484e99bb5082453110617e95420a3f8990fa8bd3
Orisabiyi <orisabiyidavid@gmail.com>
2024-06-28 11:00:45 +0100

    styled today component element and changed the section name from work to today

--- src/components/Today.jsx
+++ src/components/Today.jsx
@@ -5,13 +5,16 @@ function Today() {
     <section className={styles.section__today}>
       <h2>Join Us Today</h2>
       <p>
-        Join SellDome Today and Elevate Your Fashion Game! Ready to take your
-        fashion journey to new heights? Join SellDome today and experience the
-        future of fashion sourcing.
-        <br />
-        Whether you're a vendor looking to expand your reach or a fashion
-        designer in search of inspiration, SellDome has everything you need to
-        succeed.
+        <span>
+          Join SellDome Today and Elevate Your Fashion Game! <br /> Ready to
+          take your fashion journey to new heights? Join SellDome today and
+          experience the future of fashion sourcing.
+        </span>
+        <span>
+          Whether you're a vendor looking to expand your reach or a fashion
+          designer in search of <br /> inspiration, SellDome has everything you
+          need to succeed.
+        </span>
       </p>
     </section>
   );
