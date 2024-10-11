2fc006874bf24604b87245faf150ac286bd2b245
Gld-Mx24483 <israeltoki7@gmail.com>
2024-10-11 12:10:20 +0100

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,9 @@
 // Header.jsx
 import { useEffect, useRef, useState } from 'react';
+import Lottie from 'react-lottie-player';
+import greenLottie from './green.json';
 import styles from './Header.module.css';
+import orangeLottie from './orange.json';
 
 function Header({ onVideoPlayingChange }) {
   const [activeIndex, setActiveIndex] = useState(0);
@@ -11,14 +14,14 @@ function Header({ onVideoPlayingChange }) {
     const handleVideoEnd = () => {
       setActiveIndex(0);
       setIsVideoPlaying(false);
-      onVideoPlayingChange(false); 
+      onVideoPlayingChange(false);
     };
 
     const handleImageTimer = () => {
       if (activeIndex === 0) {
         document.querySelector(`.${styles['background-image']}`).style.animation = 'none';
         setTimeout(() => {
-          document.querySelector(`.${styles['background-image']}`).style.animation = ''; 
+          document.querySelector(`.${styles['background-image']}`).style.animation = '';
         }, 0);
 
         setTimeout(() => {
@@ -28,29 +31,65 @@ function Header({ onVideoPlayingChange }) {
           if (videoRef.current) {
             videoRef.current.play();
           }
-        }, 10000);
+        }, 20000);
       }
     };
 
     if (videoRef.current) {
-      videoRef.current.addEventListener('ended', handleVideoEnd); 
+      videoRef.current.addEventListener('ended', handleVideoEnd);
     }
 
-    handleImageTimer(); 
+    handleImageTimer();
 
     return () => {
       if (videoRef.current) {
-        videoRef.current.removeEventListener('ended', handleVideoEnd); 
+        videoRef.current.removeEventListener('ended', handleVideoEnd);
       }
     };
   }, [activeIndex, onVideoPlayingChange]);
 
+  const orangeLottiePositions = [
+    { top: '10%', left: '20%' },
+    { top: '20%', left: '50%' },
+    { top: '30%', left: '80%' },
+    { top: '5%', left: '40%' },
+    { top: '50%', left: '60%' },
+    { top: '30%', left: '20%' },
+    { top: '70%', left: '70%' },
+    { top: '80%', left: '50%' },
+    { top: '50%', left: '40%' },
+    { top: '5%', left: '60%' },
+  ];
+
   return (
     <header className={styles.header}>
       <div className={styles['background-container']}>
-        {/* Background Image */}
-        <div className={`${styles['background-item']} ${styles['background-image']} ${activeIndex === 0 ? styles.active : ''}`} />
-        {/* Background Video */}
+        <div 
+          className={`${styles['background-item']} ${styles['background-image']} ${activeIndex === 0 ? styles.active : ''}`}
+        >
+          <div className={styles['lottie-container-green']}>
+            <Lottie
+              loop
+              animationData={greenLottie}
+              play
+              style={{ width: 150, height: 150 }}
+            />
+          </div>
+          {orangeLottiePositions.map((position, index) => (
+            <div 
+              key={index}
+              className={styles['lottie-container-orange']}
+              style={{ top: position.top, left: position.left }}
+            >
+              <Lottie
+                loop
+                animationData={orangeLottie}
+                play
+                style={{ width: 50, height: 50 }}
+              />
+            </div>
+          ))}
+        </div>
         <video
           ref={videoRef}
           className={`${styles['background-item']} ${styles['background-video']} ${activeIndex === 1 ? styles.active : ''}`}
